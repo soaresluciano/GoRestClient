71e263c80fa37ec9f606ad54bac3ff0ad846657e
Luciano Soares <luciano.soares@thermofisher.com>
2020-11-08 16:46:15 +0100

    Prism basic implementation

--- GoRestClient/App.xaml.cs
+++ GoRestClient/App.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using GoRestClient.Views;
+using Prism.Ioc;
+using Prism.Unity;
 using System.Windows;
 
 namespace GoRestClient
@@ -11,7 +8,16 @@ namespace GoRestClient
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
     }
 }
---
+++ GoRestClient/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using GoRestClient.Models;
+using Prism.Mvvm;
+
+namespace GoRestClient.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private UserModel _selectedUser = null;
+        public UserModel SelectedUser
+        {
+            get => _selectedUser;
+            set => SetProperty(ref _selectedUser, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            SelectedUser = new UserModel { Name = "Dummy" };
+        }
+    }
+}
